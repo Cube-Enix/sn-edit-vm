2084b6caec5ecef495185385a91c851b0040fdca
SithLor <84148006+SithLor@users.noreply.github.com>
2022-11-12 02:06:37 -0800

    Funny

--- src/extensions/sn_ten/index.js
+++ src/extensions/sn_ten/index.js
@@ -33,9 +33,9 @@ class Ten {
       color2: '#fd8a5e',
       blocks: [
         {
-          opcode: "genhash",
+          opcode: "uuid",
           text: formatMessage({
-            id: "sn.blocks.genshash",
+            id: "sn.blocks.genuuid",
             default: "Create hash",
             description: "Make A hash ",
           }),
@@ -44,8 +44,22 @@ class Ten {
       ],
     };
   }
-  genhash() {
-    return toString(Crypto.randomUUID());
+  uuid() {
+    //https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+    //this help full beacause toString(Crypto.randomUUID()) dont work in all broswer
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
   }
 }
 
